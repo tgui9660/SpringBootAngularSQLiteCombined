ff07d77f691253ff47571056405e4ad232c4ac8b
Eric Morgan <eric@ecmorgan.net>
2025-07-03 17:52:41 -0400

    backup

--- src/main/java/com/simple/spring_angular_deployment/hibernate/WebCounterService.java
+++ src/main/java/com/simple/spring_angular_deployment/hibernate/WebCounterService.java
@@ -1,6 +1,8 @@
 package com.simple.spring_angular_deployment.hibernate;
 
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,20 @@ public class WebCounterService {
     public WebCounter saveOrUpdate(WebCounter p) {
         wcrRepository.save(p);
         return p;
+    }
+
+    public WebCounter increment(){
+        long amount = wcrRepository.count();
+
+        WebCounter toReturn = new WebCounter(1);
+
+        if (amount == 0) {
+            wcrRepository.save(toReturn);
+        }else{
+            Optional<WebCounter> found = wcrRepository.findById(1);
+            
+        }
+
+        return toReturn;
     }
 }
